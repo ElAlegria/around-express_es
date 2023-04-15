20a501d2e40c3b793f88e8af19e0cdb6d0265c83
Cristian Alegria <abraham.32v@gmail.com>
2023-04-14 22:17:24 -0600

    usear/id terminado

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const { PORT = 3000 } = process.env;
 const app = express();
-const path = require("path");
 const userCards = require("./routes/cards");
 const userUsers = require("./routes/users");
-
-app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.status(404).send({ message: "Recurso solicitado no encontrado" });
 });
+
 app.use("/users", userUsers);
 app.use("/cards", userCards);
-
-
 
 app.listen(PORT, () => {
   console.log("Enlace a server");
   // console.log(BASE_PATH);
 });
+
+//!Proximo uso(not delate)
+// const path = require("path");
+// app.use(express.static(path.join(__dirname, "public")));
